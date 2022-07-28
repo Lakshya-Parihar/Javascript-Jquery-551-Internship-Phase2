65604f38f001709ddba28229f4d3da29b3dc5170
lakshya <105096466+Lakshya-Parihar@users.noreply.github.com>
2022-07-28 21:29:46 +0530

    Add files via upload

---
+++ JsTypes.js
@@ -0,0 +1,18 @@
+function AlertExternal() {
+    alert('Hello !');
+   }
+
+   function ConfirmExternal() {
+    if(confirm('Are you Sure ?')) {
+        alert('Yes');
+    }
+    else {
+        alert('No');
+    }
+   }
+
+   function PromptExternal() {
+    var Fname=prompt('Enter Your First Name : ');
+    var Lname=prompt('Enter Your Last Name : ');
+    alert(Fname + " " + Lname);
+   }
